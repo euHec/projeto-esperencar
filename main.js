f02931591d510561957c613edc68fd899a8ac184
euHec <hectorsouzass@gmail.com>
2023-02-27 23:46:17 -0300

    Uunificando unico script

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
-import { searchCep } from './cepFunctions';
-
 const agreed = document.querySelector('#agreed');
 const button = document.querySelector('#button');
 const cep = document.querySelector('#cep');
+const rua = document.querySelector('#street');
+const cidade = document.querySelector('#city');
+const estado = document.querySelector('#state');
+const bairro = document.querySelector('#district');
+
+export const getAddress = async (cep) => {
+  const addres = await fetch(`https://cep.awesomeapi.com.br/json/${cep}`);
+  const result = await addres.json();
+  return result;
+};
+
+export const searchCep = async (value) => {
+  const {address, district, city, state} = await getAddress(value);
+  try {
+    if (!address) { throw new Error('CEP não encontrado'); }
+    rua.value = address; 
+    bairro.value = district; 
+    cidade.value = city;
+    estado.value = state;
+  } catch (error) {
+    console.log(error);
+    alert(error.message);
+  }
+};
 
 window.addEventListener("load", function() {
   const form = document.getElementById('formId');
